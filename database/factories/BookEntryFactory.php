c1cbd007e05af9d63a05a6f63688b47e6a65191f
Justin Hartwig <65766832+justin-hartwig@users.noreply.github.com>
2022-10-24 13:45:11 +0200

    Created BookEntry model with dummy data threw factory and seeding and display

---
+++ database/factories/BookEntryFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\BookEntry>
+ */
+class BookEntryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => fake()->sentence(),
+            'text' => fake()->paragraph(5),
+            'userid' => fake()->randomNumber(),
+            'released' => fake()->boolean()
+        ];
+    }
+}
