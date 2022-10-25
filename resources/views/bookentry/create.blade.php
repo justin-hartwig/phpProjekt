7802bf5f91150a423826957b8a8b5fc8def1aa25
Justin Hartwig <65766832+justin-hartwig@users.noreply.github.com>
2022-10-25 12:24:20 +0200

    Keep input on create form if error

--- resources/views/bookentry/create.blade.php
+++ resources/views/bookentry/create.blade.php
@@ -2,11 +2,11 @@
     <h2>Hier können Sie einen neuen Eintrag anlegen.<h2>
     <form method="POST" action="/bookentrys">
         @csrf
-        <input name="title" placeholder="Hier Titel eingeben...">
+        <input name="title" placeholder="Hier Titel eingeben..." value="{{old('title')}}">
         @error('title')
 		    <p class="error-text">Bitte geben Sie einen Titel ein!</p>
 	    @enderror
-        <textarea name="text" placeholder="Hier Text eingeben..."></textarea>
+        <textarea name="text" placeholder="Hier Text eingeben..." value="{{old('text')}}"></textarea>
         @error('text')
 		    <p class="error-text">Bitte geben Sie einen Text ein!</p>
 	    @enderror
