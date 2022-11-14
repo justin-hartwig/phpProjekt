5a25c5a7e5ebe2fb8b6e3d8d632cd62b44601104
Justin Hartwig <65766832+justin-hartwig@users.noreply.github.com>
2022-11-14 15:15:08 +0100

    Changed german duplicate e-mail message text

--- lang/de/validation.php
+++ lang/de/validation.php
@@ -135,7 +135,7 @@ return [
     'starts_with' => 'The :attribute must start with one of the following: :values.',
     'string' => 'The :attribute must be a string.',
     'timezone' => 'The :attribute must be a valid timezone.',
-    'unique' => 'The :attribute has already been taken.',
+    'unique' => 'Die angegebene :attribute wird schon verwendet.',
     'uploaded' => 'The :attribute failed to upload.',
     'url' => 'The :attribute must be a valid URL.',
     'uuid' => 'The :attribute must be a valid UUID.',
